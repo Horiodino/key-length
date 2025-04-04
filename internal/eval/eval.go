69fa8e63bde92ee5df12cb73755e25018c8ecbaf
Horiodino <holiodin@gmail.com>
2025-04-04 09:21:15 +0530

    updated eval.go
    Signed-off-by: Horiodino <holiodin@gmail.com>

--- internal/eval/eval.go
+++ internal/eval/eval.go
@@ -1,1 +1,30 @@
 package eval
+
+import (
+	"fmt"
+
+	"github.com/Horiodino/key-length/internal/config"
+	"github.com/Horiodino/key-length/internal/types"
+)
+
+type EvaluationResult struct {
+	Algorithm string
+	Length    int
+	Status    string
+}
+
+func EvaluateKey(key types.KeyLengthEvaluator, cfg *config.Config) *EvaluationResult {
+	algorithm, length := key.GetAlgorithm(), key.GetLength()
+	threshold := cfg.GetThreshold(algorithm)
+	isSecure := length >= threshold
+	return &EvaluationResult{
+		Algorithm: algorithm,
+		Length:    length,
+		Status: fmt.Sprintf("%s (%s)", func() string {
+			if isSecure {
+				return "Secure"
+			}
+			return "Insecure"
+		}(), cfg.SelectedStandard),
+	}
+}
