9fc495bf17035e061786505142f60c3e7a06fd65
Horiodino <holiodin@gmail.com>
2025-04-05 23:21:35 +0530

    added ExpiryWarning
    Signed-off-by: Horiodino <holiodin@gmail.com>

--- internal/eval/eval.go
+++ internal/eval/eval.go
@@ -1,22 +1,55 @@
 package eval
 
 import (
+	"crypto/x509"
 	"fmt"
+	"time"
 
 	"github.com/Horiodino/key-length/internal/config"
+	"github.com/Horiodino/key-length/internal/ecc"
+	"github.com/Horiodino/key-length/internal/rsa"
 	"github.com/Horiodino/key-length/internal/types"
 )
 
 type EvaluationResult struct {
-	Algorithm string
-	Length    int
-	Status    string
+	Algorithm     string
+	Length        int
+	Status        string
+	Expiry        string
+	ExpiryWarning string
 }
 
-func EvaluateKey(key types.KeyLengthEvaluator, cfg *config.Config) *EvaluationResult {
-	algorithm, length := key.GetAlgorithm(), key.GetLength()
+func EvaluateKey(key types.KeyLengthEvaluator, cfg *config.Config, certData []byte) *EvaluationResult {
+	length := key.GetLength()
+	var algorithm string
+
+	switch key.(type) {
+	case *rsa.RSAKey:
+		algorithm = "RSA"
+	case *ecc.ECCKey:
+		algorithm = "ECC"
+	default:
+		algorithm = "Unknown"
+	}
+
 	threshold := cfg.GetThreshold(algorithm)
 	isSecure := length >= threshold
+
+	expiry := "N/A"
+	expiryWarning := ""
+
+	if certData != nil {
+		cert, err := x509.ParseCertificate(certData)
+		if err == nil {
+			expiry = cert.NotAfter.Format("2006-01-02")
+			threshold := 90 * 24 * time.Hour
+			if time.Until(cert.NotAfter) < threshold {
+				daysLeft := int(time.Until(cert.NotAfter).Hours() / 24)
+				expiryWarning = fmt.Sprintf("Warning: Certificate expires in %d days (threshold: 90 days)", daysLeft)
+			}
+		}
+	}
+
 	return &EvaluationResult{
 		Algorithm: algorithm,
 		Length:    length,
@@ -26,5 +59,7 @@ func EvaluateKey(key types.KeyLengthEvaluator, cfg *config.Config) *EvaluationRe
 			}
 			return "Insecure"
 		}(), cfg.SelectedStandard),
+		Expiry:        expiry,
+		ExpiryWarning: expiryWarning,
 	}
 }
